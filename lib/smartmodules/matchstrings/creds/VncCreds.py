c191c80d4267e8e8b7e5a686c48265cde0aafaa6
koutto <xst3nz@gmail.com>
2019-06-30 17:46:31 +0200

    Fix & add matchstrings VncCreds

--- lib/smartmodules/matchstrings/creds/VncCreds.py
+++ lib/smartmodules/matchstrings/creds/VncCreds.py
@@ -6,10 +6,16 @@ from lib.smartmodules.matchstrings.MatchStrings import creds_match
 creds_match['vnc'] = {
 
     'hydra': {
-        '\[vnc\] host: \S+\s+login:\s+password: (?P<m1>\S*)': {
+        '\[vnc\] host: \S+\s+password: (?P<m1>\S*)': {
             'user': '',
             'pass': '$1',
         },
+    },
+    'nmap': {
+    	'Server does not require authentication': {
+    		'user': '',
+    		'pass': '',
+    	},
     },
 
 }
