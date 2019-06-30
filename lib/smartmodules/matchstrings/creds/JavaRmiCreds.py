d1ec7540696f00bc75d5623b96b23f91c3231f3b
koutto <xst3nz@gmail.com>
2019-06-30 18:49:58 +0200

    Update checks for java-rmi. Fix matchstring JavaRmiCreds

--- lib/smartmodules/matchstrings/creds/JavaRmiCreds.py
+++ lib/smartmodules/matchstrings/creds/JavaRmiCreds.py
@@ -6,7 +6,7 @@ from lib.smartmodules.matchstrings.MatchStrings import creds_match
 creds_match['java-rmi'] = {
 
     'jmxbf': {
-        'We got a valid connection for: (?P<m1>\S+):(?P<m2>\S*)': {
+        'We got a valid connection for: (?P<m1>\S*):(?P<m2>\S*)': {
             'user': '$1',
             'pass': '$2',
         },
--- lib/smartmodules/matchstrings/vulns/JavaRmiVulns.py
+++ lib/smartmodules/matchstrings/vulns/JavaRmiVulns.py
@@ -20,6 +20,9 @@ vulns_match['java-rmi'] = {
         'java_rmi_server[\s\S]*session [0-9]+ opened': 'Java RMI Server Insecure Default Configuration Java Code Execution',
         'java_jmx_server[\s\S]*session [0-9]+ opened': 'Java JMX Server Insecure Configuration Java Code Execution',
     },
+    'sjet': {
+        'Successfully loaded MBeanSiberas': 'Java JMX Server Insecure Configuration Java Code Execution',
+    },
     'ysoserial': {
         'Captured ICMP traffic:[\s\S]*ICMP echo request.*\n.*ICMP echo reply': 'Java RMI Registry Deserialization RCE',
     },
