0bffc24e60976d6cadd13612a18ebcf517ab29f9
Cyril Levis <levis.cyril@gmail.com>
2019-07-20 20:47:25 +0200

    move shodan to importer

--- lib/controller/DbController.py
+++ lib/controller/DbController.py
@@ -14,7 +14,7 @@ from lib.core.Config import *
 from lib.core.Constants import *
 from lib.core.Exceptions import FilterException
 from lib.core.NmapResultsParser import NmapResultsParser
-from lib.core.ShodanResultsParser import ShodanResultsParser
+from lib.importer.ShodanResultsParser import ShodanResultsParser
 from lib.core.Target import Target
 from lib.utils.ArgParseUtils import *
 from lib.utils.FileUtils import FileUtils
