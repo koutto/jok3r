f4275556cf2063d65fd64f0c6c10d2b69f98fce9
koutto <xst3nz@gmail.com>
2019-08-13 13:29:34 +0000

    Edit update.py with toolbox update

--- update.py
+++ update.py
@@ -8,6 +8,7 @@ import sys
 
 from lib.core.Config import *
 from lib.core.ProcessLauncher import ProcessLauncher
+from lib.core.Settings import Settings
 from lib.output.Logger import logger
 from lib.output.Output import Output
 from lib.utils.NetUtils import NetUtils
@@ -16,14 +17,12 @@ from lib.utils.NetUtils import NetUtils
 if __name__ == '__main__':
     print(BANNER)
     Output.title1('Update Jok3r')
-    logger.info("""
-        This script will update Jok3r to the latest available version.
-        It will install newly added tools in the toolbox, and will re-install
-        tools for which install/update commands have changed in settings.
-        It will also make sure to install new dependencies if needed.
-        """)
+    logger.info('This script will update Jok3r to the latest available version.')
+    logger.info('It will install newly added tools in the toolbox, and will re-install')
+    logger.info('tools for which install/update commands have changed in settings. ')
+    logger.info('It will also make sure to install new dependencies if needed.')
 
-    print
+    print()
     Output.title2('Step #0: Check Internet connection...')
     if NetUtils.is_internet_connected():
         logger.success('Connected to Internet, will continue')
@@ -31,15 +30,18 @@ if __name__ == '__main__':
         logger.error('Not connected to Internet, cannot continue !')
         sys.exit(1)
 
-    print
+    #------------------------------------------------------------------------------------
+    print()
     Output.title2('Step #1: Save current settings...')
     try:
         settings_bak = Settings()
     except Exception as e:
         logger.error('An error occured when saving current settings: {}'.format(e))
         sys.exit(1)
+    logger.success('Current settings saved')
 
-    print
+    #------------------------------------------------------------------------------------
+    print()
     Output.title2('Step #2: Update source code from master branch at ' \
         'https://github.com/koutto/jok3r ...')
     try:
@@ -50,12 +52,13 @@ if __name__ == '__main__':
         sys.exit(1)
     logger.info('git pull output:')
     print(gitoutput)
-    print
+    print()
     if gitoutput == 'Already up to date.':
-        logger.success('Jok3r is Already up-to-date !')
+        logger.success('Jok3r is already up-to-date !')
         sys.exit(0)
 
-    print
+    #------------------------------------------------------------------------------------
+    print()
     Output.title2('Step #3: Install new dependencies if needed ...')
     if 'install-dependencies.sh' in gitoutput:
         logger.info('install-dependencies.sh has been updated. Will re-run it...')
@@ -84,7 +87,8 @@ if __name__ == '__main__':
     else:
         logger.info('No change in dependencies to install')
 
-    print
+    #------------------------------------------------------------------------------------
+    print()
     Output.title2('Step #4: Perform toolbox diff...')
     if '{}{}'.format(TOOLBOX_CONF_FILE, CONF_EXT) not in gitoutput:
         logger.info('Toolbox settings ({}{}) has not been updated, no further check ' \
@@ -110,19 +114,48 @@ if __name__ == '__main__':
         for tool in diff['deleted']:
             logger.info('  +-> {}'.format(tool))
 
-    print
+        if len(diff['new']) == 0 \
+           and len(diff['updated']) == 0 \
+           and len(diff['deleted']) == 0:
+            logger.info('No change in toolbox settings detected')
+            toolbox_change = False
+        else:
+            toolbox_change = True
+
+    #------------------------------------------------------------------------------------
+    print()
     Output.title2('Step #5: Update toolbox according to the diff...')
-    logger.info('Note: This will not update all tools, just re-install tools with ' \
-        'updated configuration,')
-    logger.info('install newly added tools and delete tools removed from settings ' \
-        'if needed.')
+    if not toolbox_change:
+        logger.info('Skipped because no change in toolbox settings detected')
+    else:
+        logger.info('Note: This will not update all tools, just re-install tools with ' \
+            'updated configuration,')
+        logger.info('install newly added tools and delete tools removed from settings ' \
+            'if needed.')
 
-    if len(diff['new']) > 0:
-        logger.info('Install newly added tools...')
-        for tool in diff['new']:
-            settings_new.toolbox.install_tool(tool, fast_mode=True)
+        if len(diff['new']) > 0:
+            logger.info('Install newly added tools...')
+            for tool in diff['new']:
+                if not settings_new.toolbox.install_tool(tool, fast_mode=True):
+                    logger.error('An error occured during tool "{}" install. ' \
+                        'Exit'.format(tool))
+                    sys.exit(1)
 
-    if len(diff['updated']) > 0:
-        logger.info('Re-install tools with updated configuration...')
-        for tool in diff['updated']:
-            
+        if len(diff['updated']) > 0:
+            logger.info('Re-install tools with updated configuration...')
+            for tool in diff['updated']:
+                settings_new.toolbox.remove_tool(tool)
+                if not settings_new.toolbox.install_tool(tool, fast_mode=True):
+                    logger.error('An error occured during tool "{}" re-install. ' \
+                        'Exit'.format(tool))
+                    sys.exit(1)
+
+        if len(diff['deleted']) > 0:
+            logger.info('Delete tools removed from toolbox settings...')
+            for tool in diff['deleted']:
+                settings_bak.toolbox.remove_tool(tool)
+
+    #------------------------------------------------------------------------------------
+    print()
+    logger.success('Update script finished with success')
+    sys.exit(0)
