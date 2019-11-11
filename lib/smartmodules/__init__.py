5bdee142569bad1c2e1d63284994c92dc8a14e27
Koutto <xst3nz@gmail.com>
2019-11-11 21:33:26 +0000

    Clean some __init__ files

--- lib/smartmodules/__init__.py
+++ lib/smartmodules/__init__.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .ContextUpdater import *
-from .MatchstringsProcessor import *
-from .SmartPostcheck import *
-from .SmartStart import *
-pass
