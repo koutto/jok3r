dd02ee0b1ffdc62550c0a31fa3606734f5862be4
cbk914 <cbk914@riseup.net>
2020-07-18 20:58:34 +0200

    Minor changes

--- docker_j0k3r.sh
+++ docker_j0k3r.sh
@@ -1,1 +1,6 @@
+# CONSOLE
 sudo docker run -i -t --name jok3r-container -w /root/jok3r -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --shm-size 2g --net=host koutto/jok3r
+# GUI
+#sudo docker run -i -t -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix koutto/jok3r &
+#xhost +
+
