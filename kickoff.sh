a63bb1000670ef0ca1aa83ce7523a4c1edc3c4a8
cbk914 <cbk914@riseup.net>
2020-07-21 12:27:07 +0200

    Kick-off script update

--- kickoff.sh
+++ kickoff.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
+
+print_green() {
+    BOLD_GREEN=$(tput bold ; tput setaf 2)
+    NORMAL=$(tput sgr0)
+    echo "${BOLD_GREEN}$1${NORMAL}"
+}
+
+print_yellow() {
+    BOLD_YELLOW=$(tput bold ; tput setaf 3)
+    NORMAL=$(tput sgr0)
+    echo "${BOLD_YELLOW}$1${NORMAL}"
+}
+
+print_red() {
+    BOLD_YELLOW=$(tput bold ; tput setaf 1)
+    NORMAL=$(tput sgr0)
+    echo "${BOLD_YELLOW}$1${NORMAL}"
+}
+
+print_blue() {
+    BOLD_YELLOW=$(tput bold ; tput setaf 4)
+    NORMAL=$(tput sgr0)
+    echo "${BOLD_YELLOW}$1${NORMAL}"
+}
+
+print_delimiter() {
+    echo
+    echo "-------------------------------------------------------------------------------"
+    echo
+}
+
 clear
 python3 jok3r.py db mission
-echo "##########################################################################################"
-echo "# Kick-Off launcher                                                                       #"
-echo "##########################################################################################"
-echo -n "#Target/IP?"
+print_green
+print_delimiter
+print_green "# Kick-Off launcher                                                                      #"
+print_delimiter
+print_yellow "Target/IP?"
+print_delimiter
 echo ""
 read URL
-echo -n "Launching WAF detector on $URL and saving on database $DP..."
+print_blue "Launching WAF detector on $URL and saving on database $DP..."
 sleep 3
-python3 jok3r.py attack -t $URL --add2db $DP --profile waf-checker --fast
+python3 jok3r.py attack -t $URL -s http --add2db $DP --profile waf-checker --fast
