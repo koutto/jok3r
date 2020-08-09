f94e828e98b8f106f39b7190a989105b6f4879b7
cbk914 <cbk914@riseup.net>
2020-08-09 19:33:38 +0200

    Bug fix

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 clear
 git pull
 ./install-dependencies.sh
-python3 jok3r.py toolbox --update-all --fast
-python3 jok3r.py toolbox --install-all --fast
+python3 jok3r.py toolbox --update-all --auto
+python3 jok3r.py toolbox --install-all --auto
 python3 jok3r.py toolbox --show-all
